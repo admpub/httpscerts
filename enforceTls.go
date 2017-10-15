dd53a892b70b41656ed7c63b143a3bac66d2c023
jEzEk <jezek@chicki.sk>
2017-10-15 19:24:02 +0200

    refactoring

--- enforceTls.go
+++ enforceTls.go
@@ -3,14 +3,8 @@ package https
 import "net/http"
 
 func EnforceTLS(h http.Handler) http.Handler {
-	return enforceTLS{h}
-}
-
-type enforceTLS struct {
-	http.Handler
-}
-
-func (h enforceTLS) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
-	h.Handler.ServeHTTP(w, r)
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Strict-Transport-Security", "max-age=63072000; includeSubDomains")
+		h.ServeHTTP(w, r)
+	})
 }
